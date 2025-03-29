d470510bbb7a18b194510d3e1dfe77a230d66ec1
Hu Gang <18768366022@163.com>
2025-03-29 17:47:11 +0800

    chore: remove preload.ts

--- src/main/preload.ts
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron'
-
-console.log('Hello Preload!')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openFile: () => ipcRenderer.invoke('dialog:openFile'),
-  readFile: () => ipcRenderer.invoke('file:readFile'),
-})
